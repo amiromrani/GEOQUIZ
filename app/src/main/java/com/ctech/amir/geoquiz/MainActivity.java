dfbafc05a61e369c83be97b9fab616784490b4b8
amiromrani <46926811+amiromrani@users.noreply.github.com>
2019-02-28 08:43:57 -0600

    look at main.xml

--- app/src/main/java/com/ctech/amir/geoquiz/MainActivity.java
+++ app/src/main/java/com/ctech/amir/geoquiz/MainActivity.java
@@ -2,8 +2,14 @@ package com.ctech.amir.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mTrueButton;
+    private Button mFalseButton;
+
+    public MainActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
