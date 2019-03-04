5eaec163170a46e9e626b1cea9533e79f08cbd0d
amiromrani <46926811+amiromrani@users.noreply.github.com>
2019-03-04 08:52:36 -0600

    look at main.xml

--- app/src/main/java/com/ctech/amir/geoquiz/MainActivity.java
+++ app/src/main/java/com/ctech/amir/geoquiz/MainActivity.java
@@ -2,7 +2,9 @@ package com.ctech.amir.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button mTrueButton;
@@ -15,6 +17,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mFalseButton  = (Button) findViewById(R.id.false_button);
+
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(:MainActivity.this,
+                        R.string.correct_toast,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(:MainActivity.this,
+                        R.string.incorrect_toast,
+                        Toast.LENGTH_SHORT).show();
+                }
+            });
+
 
 
 
